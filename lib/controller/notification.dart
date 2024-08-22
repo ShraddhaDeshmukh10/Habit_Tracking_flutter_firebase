7f7df87061775207b052ecf3af1cf7935ef6ec98
ShraddhaDeshmukh10 <shraddha10122002@gmail.com>
2024-08-22 13:35:56 +0530

    notification.dart

--- lib/controller/notification.dart
+++ lib/controller/notification.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// class NotificationService {
-//   final FlutterLocalNotificationsPlugin _notificationsPlugin =
-//       FlutterLocalNotificationsPlugin();
+class NotificationService {
+  final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-//   Future<void> init() async {
-//     const AndroidInitializationSettings initializationSettingsAndroid =
-//         AndroidInitializationSettings('@mipmap/ic_launcher');
-//     final InitializationSettings initializationSettings =
-//         InitializationSettings(android: initializationSettingsAndroid);
-//     await _notificationsPlugin.initialize(initializationSettings);
-//   }
+  Future<void> init() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await _notificationsPlugin.initialize(initializationSettings);
+  }
 
-//   Future<void> showNotification(String title, String body) async {
-//     const AndroidNotificationDetails androidPlatformChannelSpecifics =
-//         AndroidNotificationDetails(
-//             'habit_tracker_channel_id', 'Habit Tracker Channel',
-//             importance: Importance.max, priority: Priority.high);
-//     const NotificationDetails platformChannelSpecifics =
-//         NotificationDetails(android: androidPlatformChannelSpecifics);
-//     await _notificationsPlugin.show(0, title, body, platformChannelSpecifics);
-//   }
-// }
+  Future<void> showNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+            'habit_tracker_channel_id', 'Habit Tracker Channel',
+            importance: Importance.max, priority: Priority.high);
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await _notificationsPlugin.show(0, title, body, platformChannelSpecifics);
+  }
+}
